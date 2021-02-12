b1a177d3c50dd05a4f99f40a47e54aca11f95adc
Cayo-SMN <cayo@smn.com.br>
2021-02-12 16:33:09 -0300

    SPRINT001-2021: Inicio do projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PastelariaMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PastelariaMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Exemplo
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var url = "http://localhost:5000/api/usuario/gestor/"+id.ToString();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            HttpResponseMessage response = await client.GetAsync(url);
+            Usuario usuarioResult;
+            string result;
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+                usuarioResult = JsonConvert.DeserializeObject<Usuario>(result);
+                Console.WriteLine(usuarioResult);
+                return View(usuarioResult);
+            }
+            else
+            {
+                result = "Deu merda";
+            }
+            client.Dispose();
+            return View();
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PastelariaMvc.Models
+{
+    
+    public class Usuario
+    {
+        public short IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Senha { get; set; }
+        public bool EstaAtivo { get; set; } = true;
+        public int IdEndereco { get; set; }
+        //public Endereco Endereco { get; set; }
+        public short IdEmail { get; set; }
+        //public Email Email { get; set; }
+        public short IdTelefone { get; set; }
+        //public Telefone Telefone { get; set; }
+
+        /*
+        public Usuario()
+        {
+            this.Email = new Email();
+            this.Endereco = new Endereco();
+            this.Telefone = new Telefone();
+        }
+        */
+
+        //public List<Tarefa> Tarefas { get; set; }
+
+        public Usuario()
+        {
+
+        }
+            
+    }
+}

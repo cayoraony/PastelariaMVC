85e29a0686cc716c8768f533bcf9f5ce8a93eaed
Cayo-SMN <cayo@smn.com.br>
2021-03-11 09:41:56 -0300

    SPRINT005-2021: Validation nas tarefas concluída

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -7,23 +7,27 @@ namespace PastelariaMvc.Models
     {
         public int IdTarefa { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "Data Limite")]
         public DateTime DataLimite { get; set; }
         public DateTime DataCadastro { get; set; } = DateTime.Now;
         public DateTime? DataConclusao { get; set; } = null;
         public DateTime? DataCancelada { get; set; } = null;
 
-        [Display(Name = "Gestor")]
         public int IdGestor { get; set; }
         public Usuario Gestor { get; set; }
 
+
+        [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "Subordinado")]
         public int IdSubordinado{ get; set; }
         public Usuario Subordinado { get; set; }
-        
+
+        [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "Status")]
         public int IdStatusTarefa { get; set; }
         public StatusTarefa Status { get; set; }

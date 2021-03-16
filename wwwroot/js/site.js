9c6aa13727ebb399dc6dd4af7341c0098119ae92
Joao Marcos <joaomarcos11.tech@gmail.com>
2021-03-16 11:50:46 -0300

    SPRINT006-2021: imporntando link js e css para layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready( function () {
+    $('#ListarTarefas').DataTable( {
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.22/i18n/Portuguese-Brasil.json"
+        }
+    } );
+});
+
+// Consulta a API ViaCEP
+$("#cep").blur(function () {
+    var cep = this.value.replace(/[^0-9]/, "");
+
+    if (cep.length != 8) {
+        return false;
+    }
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    $.getJSON(url, function (dadosRetorno) {
+        try {
+            $("#endereco").val(dadosRetorno.logradouro);
+            $("#bairro").val(dadosRetorno.bairro);
+            $("#cidade").val(dadosRetorno.localidade);
+            $("#uf").val(dadosRetorno.uf);
+        } catch (ex) { }
+    });
+});
+
+// Mascaras
+$(document).ready(function () {
+    $('.inputCep').mask('00000-000');
+    // $('.inputDDD').mask('00');
+});
+
+function unMask() {
+    $('.inputCep').unmask();
+    // $('.inputTel').unmask();
+    // $('.inputDDD').unmask();
+}
+

93ea8b6884614a33fbe1e117dec0c6fb3f84b543
Joao Marcos <joaomarcos11.tech@gmail.com>
2021-03-17 09:21:18 -0300

    SPRINT006-2021: concentrando scripts e styles em arquivos unicos

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,7 +4,7 @@
 // Write your JavaScript code.
 
 $(document).ready( function () {
-    $('#ListarTarefas').DataTable( {
+    $('#HomeGestor, #ListarTarefas, #VerTodas').DataTable( {
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.22/i18n/Portuguese-Brasil.json"
         }
